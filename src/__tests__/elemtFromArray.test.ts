dab7c8f151c6e12c3916c0977b8b978e5e56eba7
Orche <orkv1234@gmail.com>
2023-09-20 19:40:09 +0300

    fixed unit testing

--- src/__tests__/elemtFromArray.test.ts
+++ src/__tests__/elemtFromArray.test.ts
@@ -3,12 +3,12 @@ import random from '..';
 
 describe('elemtFromArray', () => {
     it('should return undefined for an empty array', () => {
-        expect(random.elemtFromArray([])).toBeUndefined();
+        expect(random.elementFromArray([])).toBeUndefined();
     });
 
     it('should return a random element from the array', () => {
         const array = [1, 2, 3, 4, 5];
-        const randomElement = random.elemtFromArray(array);
+        const randomElement = random.elementFromArray(array);
         expect(array).toContain(randomElement);
     });
 });
